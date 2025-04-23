af5efd90f6824fc379cd2530dbe54bad1fe67bdc
Arthur Lluisma <ajclluisma@gmail.com>
2025-04-23 19:17:27 +0800

    Setup player movement

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -3,9 +3,16 @@ extends CharacterBody3D
 @export_group("Camera")
 @export_range(0.0, 1.0) var mouse_sensitivity = 0.25
 
+@export_group("Movement")
+@export var move_speed = 8.0
+@export var acceleration = 20.0
+
+
 var _camera_input_direction = Vector2.ZERO
 
 @onready var _camera_pivot: Node3D = %CameraPivot
+@onready var _camera: Camera3D = %Camera3D
+
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("left_click"):
@@ -30,3 +37,14 @@ func _physics_process(delta: float) -> void:
 	_camera_pivot.rotation.y -= _camera_input_direction.x * delta
 	
 	_camera_input_direction = Vector2.ZERO
+	
+	var raw_input = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var forward = _camera.global_basis.z
+	var right = _camera.global_basis.x
+	
+	var move_direction = forward * raw_input.y + right * raw_input.x
+	move_direction.y = 0.0
+	move_direction = move_direction.normalized()
+	
+	velocity = velocity.move_toward(move_direction * move_speed, acceleration * delta)
+	move_and_slide()
