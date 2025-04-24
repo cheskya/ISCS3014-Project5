6f6bd6bcc98f3934bb9159f583c672c316a78f97
Nina Sandejas <nina.sandejas@student.ateneo.edu>
2025-04-24 22:01:56 +0800

    Fixed bullet and added enemy explosion.

--- scenes/enemy/enemy.gd
+++ scenes/enemy/enemy.gd
@@ -4,6 +4,7 @@ var lives = 3
 signal enemy_killed(enemy)
 
 @onready var label = $Label3D
+var explosion_scene = preload("res://scenes/explosion.tscn") 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -19,4 +20,9 @@ func _on_hit_box_body_entered(body: Node3D) -> void:
 	lives -= 1
 	label.text = "lives: " + str(lives)
 	if (lives <= 0):
+		var explosion = explosion_scene.instantiate()
+		get_parent().add_child(explosion)
+		explosion.global_transform.origin = global_transform.origin
+
 		enemy_killed.emit(self)
+		queue_free() 
