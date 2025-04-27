9502f37bbedd84ec7fe82eb167617ae561f5ed0b
Chesca <fdjr401@gmail.com>
2025-04-27 19:07:06 +0800

    Refactor code for enemy

--- scenes/enemy/enemy.gd
+++ scenes/enemy/enemy.gd
@@ -1,38 +1,33 @@
 extends StaticBody3D
 
 var lives = 3
-var has_exploded = false
-signal enemy_killed(enemy)
+var is_killed = false
 
 @onready var label = $Label3D
 @onready var hitbox = $HitBox
-@onready var explosion = $GPUParticles3D
-@onready var explosion_timer = $ExplosionTimer
+@onready var explosion = $Explosion
 @onready var body = $Body
+
 
 func _ready():
 	explosion.emitting = false
 
+
 func _on_hit_box_area_entered(area: Area3D) -> void:
-	if has_exploded:
+	if is_killed:
 		return
 
 	lives -= 1
 	label.text = "lives: " + str(lives)
 
 	if lives <= 0:
-		has_exploded = true
+		is_killed = true
 		hitbox.set_deferred("monitoring", false)
-		explode()
+		body.hide()
+		label.hide()
+		explosion.emitting = true
 
 
-func explode():
-	body.hide()
-	label.hide()
-	explosion.emitting = true
-	explosion_timer.start()
-
-
-func _on_explosion_timer_timeout() -> void:
+func _on_explosion_finished() -> void:
 	get_parent().remove_child(self)
 	queue_free()
